34e2b4aa9ddbfb638e868711654952860d04c35d
tim-speight <60938034+tim-speight@users.noreply.github.com>
2020-06-21 18:45:19 +0100

    Create red.sh

---
+++ red.sh
@@ -0,0 +1,4 @@
+sudo npm install -g raspi-io --unsafe-perm --force
+sudo npm install -g node-red-contrib-gpio --unsafe-perm --force
+sudo npm install -g node-red-contrib-admin --unsafe-perm
+sudo npm install -g node-red-dashboard --unsafe-perm
