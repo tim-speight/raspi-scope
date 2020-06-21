0527071c1fc0cc26b19b3b509f512efc802b1b4e
tim-speight <60938034+tim-speight@users.noreply.github.com>
2020-06-21 17:34:01 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+sudo wget https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/nodered.service -O /lib/systemd/system/nodered.service
+sudo wget https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/node-red-start -O /usr/bin/node-red-start
+sudo wget https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/node-red-stop -O /usr/bin/node-red-stop
+sudo chmod +x /usr/bin/node-red-st*
+sudo systemctl daemon-reload
